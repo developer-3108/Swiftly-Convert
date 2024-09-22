f2816d858fb1c71c06e3ee361960299604cdfd72
Akshat Srivastava <developer-3108@users.noreply.github.com>
2024-09-22 12:31:53 +0530

    Initial Commit

---
+++ SwiftConvert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConvert
+//
+//  Created by Akshat Srivastava on 22/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConvert/SwiftConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConvertApp.swift
+//  SwiftConvert
+//
+//  Created by Akshat Srivastava on 22/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
